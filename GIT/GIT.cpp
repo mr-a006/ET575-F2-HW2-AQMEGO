6a7945332d0fb40539153ad78b7a33ba684853c6
mr-a006 <aqeelhussain2006.02@gmail.com>
2025-09-12 20:23:48 +0000

    Aqeel Hussain Task A, B, C

--- GIT/GIT.cpp
+++ GIT/GIT.cpp
@@ -3,14 +3,17 @@
 int main() {
     std::cout << "Gonzalo Task A\n";
     std::cout << "Melissa Task A\n";
+    std::cout << "Aqeel Task A\n";
 
 
     std::cout << "Gonzalo Task B\n";
     std::cout << "Melissa Task B\n";
+    std::cout << "Aqeel Task B\n";
 
 
     std::cout << "Gonzalo Task C\n";
     std::cout << "Melissa Task C\n";
+    std::cout << "Aqeel Task C\n";
 
 
 
