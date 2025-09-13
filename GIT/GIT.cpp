cdd59a712941998410f4bb5711839aea94bd6107
Gongg0910 <chefgonzalonyc@gmail.com>
2025-09-13 17:46:30 +0000

    Please work...

---
+++ GIT/GIT.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Gonzalo Task A\n";
+    std::cout << "Melissa Task A\n";
+    std::cout << "Aqeel Task A\n";
+
+
+    std::cout << "Gonzalo Task B\n";
+    std::cout << "Melissa Task B\n";
+    std::cout << "Aqeel Task B\n";
+
+
+    std::cout << "Gonzalo Task C\n";
+    std::cout << "Melissa Task C\n";
+    std::cout << "Aqeel Task C\n";
+
+
+
+    return 0;
+}
